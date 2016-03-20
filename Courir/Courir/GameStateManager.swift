0d3b7227c77a5620e0210d9fbd7c53b1ffa19905
ianngiaw <ianngiawtingan@gmail.com>
2016-03-20 17:00:51 +0800

    Added basic implementation of gamestatemanager methods

--- Courir/Courir/GameStateManager.swift
+++ Courir/Courir/GameStateManager.swift
@@ -9,5 +9,37 @@
 import Foundation
 
 class GameStateManager {
+    let state = GameState()
     
+    func updateGameSpeed(timeStep: Int) {
+        state.currentSpeed = initialGameSpeed + Int(Double(timeStep) * gameAcceleration)
+    }
+    
+    func updateObstaclePositions() {
+        for obstacle in state.obstacles {
+            obstacle.xCoordinate -= state.currentSpeed
+        }
+        // Remove obstacles that have gone off-screen
+        state.obstacles = state.obstacles.filter{$0.xCoordinate + $0.xWidth - 1 >= 0}
+    }
+    
+    func insertObstacle(obstacle: Obstacle) {
+        state.obstacles.append(obstacle)
+    }
+    
+    func insertPlayer(player: Player) {
+        state.players.append(player)
+    }
+    
+    var score: Int {
+        return state.distance
+    }
+    
+    var speed: Int {
+        return state.currentSpeed
+    }
+    
+    var gameState: GameState {
+        return state
+    }
 }
