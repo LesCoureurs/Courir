9c8a7fe5d82ac331c1ac78303c4588db493562c6
ianngiaw <ianngiawtingan@gmail.com>
2016-03-23 15:43:59 +0800

    Removed magic probabilities

--- Courir/Courir/Constants.swift
+++ Courir/Courir/Constants.swift
@@ -15,6 +15,9 @@ let unitsPerGameGridCell: Int = actualGridSize / gameGridSize
 let initialGameSpeed = 60
 let speedMultiplier = 0.337768 * 5
 
+let floatingProbability = Float(0.25)
+let nonfloatingProbability = Float(0.25)
+
 let jumpDuration = 0.6
 let duckDuration = 0.6
 let invulnerableDuration = 1.0
