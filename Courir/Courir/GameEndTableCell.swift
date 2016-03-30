de22b1749336b4ff51ffe8d9ade3949379525e8b
joeleba <hieu.giang.2810@gmail.com>
2016-03-31 01:08:59 +0800

    GameEndTableCell

---
+++ Courir/Courir/GameEndTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameEndTableCell
+//  Courir
+//
+//  Created by Hieu Giang on 31/3/16.
+//  Copyright © 2016 NUS CS3217. All rights reserved.
+//
+
+import UIKit
+
+class GameEndTableCell: UITableViewCell {
+    var label: UILabel?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
