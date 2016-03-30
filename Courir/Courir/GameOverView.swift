5231babc2be97e6b14335ce1376364742add8f43
joeleba <hieu.giang.2810@gmail.com>
2016-03-30 23:18:03 +0800

    added GameOverView

---
+++ Courir/Courir/GameOverView.swift
@@ -0,0 +1,13 @@
+//
+//  GameOverView.swift
+//  Courir
+//
+//  Created by Hieu Giang on 30/3/16.
+//  Copyright © 2016 NUS CS3217. All rights reserved.
+//
+
+import UIKit
+
+class GameOverView: UIView {
+
+}
