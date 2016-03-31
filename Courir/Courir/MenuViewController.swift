10dd2c602ef7e8f90f14ef8c877cafd9998ad199
joeleba <hieu.giang.2810@gmail.com>
2016-04-01 02:41:27 +0800

    ask for name

--- Courir/Courir/MenuViewController.swift
+++ Courir/Courir/MenuViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 import SpriteKit
 
 class MenuViewController: UIViewController {
-
+    private var nameTextField: UITextField?
+    private var saveAction: UIAlertAction?
+    
     private let menuOptions = ["Play", "Multiplayer"]
     @IBOutlet var menuButtons: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if myName == nil {
+            askForName()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,12 +51,49 @@ class MenuViewController: UIViewController {
         
     }
 
-     // MARK: - Navigation
+    // MARK: - Navigation
 
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startGameSegue" {
             let destination = segue.destinationViewController as! GameViewController
             destination.isMultiplayer = false
         }
-     }
+    }
+    
+    // MARK: Player Name
+    private func askForName() {
+        let title = "Enter Name"
+        let message = ""
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        alertController.addTextFieldWithConfigurationHandler({ (textField) -> Void in
+            textField.placeholder = myDeviceName
+            self.nameTextField = textField
+            self.nameTextField!.addTarget(self, action: #selector(MenuViewController.textFieldDidChange(_:)), forControlEvents: .EditingChanged)
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            alertController.dismissViewControllerAnimated(true, completion: nil)
+        }
+        alertController.addAction(cancelAction)
+        
+        saveAction = UIAlertAction(title: "Save", style: .Default) { action -> Void in
+            myName = self.nameTextField?.text
+        }
+        alertController.addAction(saveAction!)
+        disableSaveIfEmptyField()
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func textFieldDidChange(textField: UITextField) {
+        disableSaveIfEmptyField()
+    }
+    
+    private func disableSaveIfEmptyField() {
+        guard let textContent = nameTextField?.text else {
+            return
+        }
+        saveAction?.enabled = !textContent.isEmpty
+    }
 }
