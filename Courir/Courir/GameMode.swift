42ba095003f1bcc36b9c5fae5c3e44edbc1aa2c6
Karen Ang <thekarenang@gmail.com>
2016-04-05 09:00:33 +0800

    Add GameMode

---
+++ Courir/Courir/GameMode.swift
@@ -0,0 +1,13 @@
+//
+//  GameMode.swift
+//  Courir
+//
+//  Created by Karen on 5/4/16.
+//  Copyright © 2016 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+enum GameMode {
+    case SinglePlayer, Multiplayer, SpecialMultiplayer
+}
