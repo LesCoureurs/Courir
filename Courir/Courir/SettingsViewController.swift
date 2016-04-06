cfe5136f1a6fad1ba2f5521e36887f6361df9d40
Karen Ang <thekarenang@gmail.com>
2016-04-06 15:52:56 +0800

    Hide status bar for settings

--- Courir/Courir/SettingsViewController.swift
+++ Courir/Courir/SettingsViewController.swift
@@ -54,7 +54,11 @@ class SettingsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
 
     /*
     // MARK: - Navigation
