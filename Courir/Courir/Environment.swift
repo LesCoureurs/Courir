d28fcd3fc5cdbdec20d41fe4073008bb4cf2792d
Sebastian Quek <sebq13@gmail.com>
2016-04-08 15:18:33 +0800

    Increased environment removal x coordinate

--- Courir/Courir/Environment.swift
+++ Courir/Courir/Environment.swift
@@ -10,7 +10,7 @@ class Environment: GameObject {
     static let spawnXCoordinate = 15 * unitsPerGameGridCell
     static let spawnYCoordinate = -66 * unitsPerGameGridCell
     static let spawnXCoordinateIncrement = 65 * unitsPerGameGridCell
-    static let removalXCoordinate = -45 * unitsPerGameGridCell
+    static let removalXCoordinate = -43 * unitsPerGameGridCell
     static let lowestZPosition: CGFloat = -99999
     
     static var numEnvironmentObjects = 0
