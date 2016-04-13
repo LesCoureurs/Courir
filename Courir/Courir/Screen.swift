6521895b2b6221a9f3a56933b948d48cbb8b38d1
Karen Ang <thekarenang@gmail.com>
2016-04-13 23:43:28 +0800

    Add Screen enum

---
+++ Courir/Courir/Screen.swift
@@ -0,0 +1,19 @@
+//
+//  Screen.swift
+//  Courir
+//
+//  Created by Karen on 13/4/16.
+//  Copyright © 2016 NUS CS3217. All rights reserved.
+//
+
+import Foundation
+
+enum Screen: String {
+    case Main = "Main"
+    case Menu = "Menu"
+    case Settings = "Settings"
+    case SinglePlayer = "SinglePlayerStart"
+    case Multiplayer = "Game"
+    case Room = "Room"
+    case RoomSelection = "RoomSelection"
+}
