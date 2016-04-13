9306bd6030d20e0ac40115f26b540a485dbbf75a
Karen Ang <thekarenang@gmail.com>
2016-04-14 01:50:59 +0800

    Remove commented out code

--- Courir/Courir/GameViewController.swift
+++ Courir/Courir/GameViewController.swift
@@ -132,15 +132,4 @@ class GameViewController: UIViewController {
             presentGameScene()
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Courir/Courir/MainViewController.swift
+++ Courir/Courir/MainViewController.swift
@@ -103,17 +103,6 @@ class MainViewController: UIViewController {
             contentView.addSubview(viewController.view)
             viewController.didMoveToParentViewController(self)
         }
-    }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    }    
 }
 
