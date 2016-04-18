0ca5a570fc1a9f89e11a45b4977ac1bbe0ca1629
ianngiaw <ianngiawtingan@gmail.com>
2016-04-18 15:45:23 +0800

    Added comments to EventQueue

--- Courir/Courir/EventQueue.swift
+++ Courir/Courir/EventQueue.swift
@@ -9,9 +9,11 @@
 import Foundation
 
 class EventQueue {
+    /// The type of an item in the `EventQueue`
     typealias Element = (event: GameEvent, playerNumber: Int, timeStep: Int, otherData: AnyObject?)
     private var queue = [Element]()
     
+    /// The head of the `EventQueue`, the `Element` in the queue with the highest priority.
     var head: Element? {
         get {
             guard count > 0 else {
@@ -21,10 +23,12 @@ class EventQueue {
         }
     }
     
+    /// The number of items in the `EventQueue`.
     var count: Int {
         return queue.count
     }
     
+    /// Initializes the `EventQueue` with an initial list of `Element`s.
     convenience init(initialEvents: [Element]) {
         self.init()
         queue = initialEvents
@@ -33,11 +37,16 @@ class EventQueue {
         }
     }
     
+    /// Inserts an `Element` into the `EventQueue` with the provided `event`, `playerNumber`,
+    /// `timeStep` and any `otherData`.
     func insert(event: GameEvent, playerNumber: Int, timeStep: Int, otherData: AnyObject?) {
         queue.append((event, playerNumber, timeStep, otherData))
         shiftUp(count - 1)
     }
     
+    /// Removes the `head` from the `EventQueue`.
+    /// - Returns: The `Element` that was at the head of the queue.
+    /// If the queue is empty, returns `nil`.
     func removeHead() -> Element? {
         guard count > 0 else {
             return nil
@@ -52,17 +61,23 @@ class EventQueue {
         return head
     }
     
+    /// Gets the parent index for an item at `index` in the `queue` array.
+    /// `index` must be greater than `0`.
     private func getParentIndex(index: Int) -> Int {
         assert(index > 0)
         return (index + 1) / 2 - 1
     }
     
+    /// Gets the child indexes for an item at `index` in the `queue` array.
+    /// - Returns: A 2-tuple with left (the index of the left child) and
+    /// right (the index of the right child).
     private func getChildIndexes(index: Int) -> (left: Int, right: Int) {
         let left = (index + 1) * 2 - 1
         let right = (index + 1) * 2
         return (left, right)
     }
     
+    /// Shifts the `Element` found at `index` in the `queue` array up the `Element` is misplaced.
     private func shiftUp(index: Int) {
         guard index > 0 else {
             return
@@ -76,6 +91,7 @@ class EventQueue {
         }
     }
     
+    /// Shifts the `Element` found at `index` in the `queue` array down the `Element` is misplaced.
     private func shiftDown(index: Int) {
         let childIndexes = getChildIndexes(index)
         let currentVal = queue[index].timeStep
@@ -97,6 +113,7 @@ class EventQueue {
         shiftDown(indexToSwap)
     }
     
+    /// Swaps `indexA` and `indexB` in the `queue` array.
     private func swap(indexA a: Int, indexB b: Int) {
         let aData = queue[a]
         queue[a] = queue[b]
